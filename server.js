f7cef1bf32519218625670ffe96b0f8bc4732835
chambrig <54903181+chambrig@users.noreply.github.com>
2019-11-13 21:29:37 -0500

    updating npm

--- server.js
+++ server.js
@@ -2,10 +2,28 @@ const express = require('express');
 const fetch = require('node-fetch');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+app.get('/establishments', (req, res) => {
+  const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$select=distinct establisment_id';
+  fetch(baseURL)
+    .then((r) => r.json())
+    .then((data) => {
+      console.log(data)
+      // data = data.filter(course => course.course_id.includes("INST"))
+      // console.log(data)
+      // res.send({ data: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/error');
+    });
+});
 
 app.get('/api', (req, res) => {
   const baseURL = 'https://api.umd.io/v0/courses/list?semester=201908';
