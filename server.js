cb240da9689fe32f5a6b950bb8b1f4833920bbe5
chambrig <54903181+chambrig@users.noreply.github.com>
2019-11-18 13:28:33 -0500

    Testing server

--- server.js
+++ server.js
@@ -10,18 +10,18 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/establishments', (req, res) => {
-  const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$'+
-  'query=SELECT DISTINCT establishment_id, name, category, city, state, zip,' +
-  'address_line_1, address_line_2, owner '+
-  ''+
-  'ORDER BY establishment_id ASC ' +
-  'LIMIT 50000';
-
-  const baseURL2 = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$'+
-  'query=SELECT establishment_id, max(inspection_date) '+
-  'GROUP BY establishment_id '+
-  'ORDER BY establishment_id ASC ' +
-  'LIMIT 50000';
+  // const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$'+
+  // 'query=SELECT DISTINCT establishment_id, name, category, city, state, zip,' +
+  // 'address_line_1, address_line_2, owner '+
+  // ''+
+  // 'ORDER BY establishment_id ASC ' +
+  // 'LIMIT 50000';
+  //
+  // const baseURL2 = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$'+
+  // 'query=SELECT establishment_id, max(inspection_date) '+
+  // 'GROUP BY establishment_id '+
+  // 'ORDER BY establishment_id ASC ' +
+  // 'LIMIT 50000';
 
   // dataDistinct = fetch(baseURL2)
   //   .then((r) => r.json())
@@ -36,6 +36,8 @@ app.get('/establishments', (req, res) => {
   //   //   res.redirect('/error');
   //   // });
   //
+  const baseURL2 = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$';
+
   fetch(baseURL2)
     .then((r) => r.json())
     .then((data) => {
