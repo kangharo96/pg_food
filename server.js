2025242fc58fca3069b3f2a77113ca44c219e6cf
Farhanahmedinst377 <54903462+Farhanahmedinst377@users.noreply.github.com>
2019-11-26 15:04:21 -0500

    New branch_need to merge

--- server.js
+++ server.js
@@ -1,87 +1,31 @@
 const express = require('express');
 const fetch = require('node-fetch');
-const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 8080;
-const appToken = "T4fTY4mWpk91TTTOVWnEPNaI4";
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
-// previous getURLS im not ready to part with - Colin
-// app.get('/establishments', (req, res) => {
-  // const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$'+
-  // 'query=SELECT DISTINCT establishment_id, name, category, city, state, zip,' +
-  // 'address_line_1, address_line_2, owner '+
-  // ''+
-  // 'ORDER BY establishment_id ASC ' +
-  // 'LIMIT 50000';
-  //
-  // const baseURL2 = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$'+
-  // 'query=SELECT establishment_id, max(inspection_date) '+
-  // 'GROUP BY establishment_id '+
-  // 'ORDER BY establishment_id ASC ' +
-  // 'LIMIT 50000';
-
-  // const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$'+
-  // 'query=SELECT establishment_id, max(inspection_date)' +
-  // 'GROUP BY establishment_id '+
-  // 'ORDER BY establishment_id ASC ' +
-  // 'LIMIT 50000&' + '$$app_token=' + appToken;
-// });
-
-/*
-  IMPORTANT For some reason this endpoint can NEVER be called /establishments,
-  literally had me working on this for hours, fml - Colin
-  In other news, this gets all distinct establishments from the data and their
-  latest inspection date.
-*/
-app.get('/allEstablishments', (req, res) => {
-  /*
-    ~11/20/19 Colin Hambright~
-    New request URL gets every column and orders by establishment_id and
-    inspection_date. Limit will need to change so that we can paginate data
-    apiToken comes from my personal developer account with SODA.
-  */
+app.get('/establishments', (req, res) => {
+  const appToken = "T4fTY4mWpk91TTTOVWnEPNaI4";
   const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$'+
-  'query=SELECT * ' +
-  'ORDER BY establishment_id ASC, inspection_date DESC ' +
-  'LIMIT 50000&' + '$$app_token=' + appToken;
-
+  'query=SELECT establishment_id, name, owner, category, city, state, inspection_date, inspection_type, inspection_results ' +
+  'ORDER BY establishment_id ASC ' +
+  'LIMIT 5000&' + '$$app_token=' + appToken;
   fetch(baseURL)
     .then((r) => r.json())
     .then((data) => {
-      /*
-        ~11/20/19 Colin Hambright~
-        Grab the first instance of a establishment and add it to a new array.
-        This relies on the url query ordering the establishments by their IDs..
-        and by their inspection dates.
-      */
-      let dataDistinct = [];
-      let previousId = 0;
-      for(row = 0; row < data.length; row++){
-        let currId = parseInt(data[row]["establishment_id"], 10);
-        if(data[row]["establishment_id"] > previousId){
-          previousId = currId;
-          dataDistinct.push(data[row]);
-        }
-      }
-      console.log(dataDistinct);
-      res.send({ data: dataDistinct });
+     
+      res.status(200).json({ data: data });
     })
     .catch((err) => {
-      console.log(err);
+       res.status(500).json({'message': 'Opps!! something went wrong'})
       //res.redirect('/error');
     });
 
 });
-
-// app.post('/establishment', (req, res) => {
-//
-//
-// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
